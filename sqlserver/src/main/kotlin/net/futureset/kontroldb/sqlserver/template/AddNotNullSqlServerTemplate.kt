cf85c71baaa98079111c5e2cf23203052086caac
futureset <github@futureset.net>
2023-10-18 21:12:35 +0100

    correct sql templates

--- sqlserver/src/main/kotlin/net/futureset/kontroldb/sqlserver/template/AddNotNullSqlServerTemplate.kt
+++ sqlserver/src/main/kotlin/net/futureset/kontroldb/sqlserver/template/AddNotNullSqlServerTemplate.kt
@@ -1,11 +1,14 @@
 package net.futureset.kontroldb.sqlserver.template
 
 import net.futureset.kontroldb.DbAwareTemplate
+import net.futureset.kontroldb.SqlTemplate
 import net.futureset.kontroldb.TemplatePriority
 import net.futureset.kontroldb.modelchange.AddNotNull
 import net.futureset.kontroldb.settings.EffectiveSettings
+import org.koin.core.annotation.Singleton
 import kotlin.reflect.KClass
 
+@Singleton(binds=[SqlTemplate::class])
 class AddNotNullSqlServerTemplate(db: EffectiveSettings) : DbAwareTemplate<AddNotNull>(db, TemplatePriority.DATABASE) {
     override fun type(): KClass<AddNotNull> {
         return AddNotNull::class
--- sqlserver/src/main/kotlin/net/futureset/kontroldb/sqlserver/template/DropIfExistsSqlServerTemplate.kt
+++ sqlserver/src/main/kotlin/net/futureset/kontroldb/sqlserver/template/DropIfExistsSqlServerTemplate.kt
@@ -1,11 +1,14 @@
 package net.futureset.kontroldb.sqlserver.template
 
 import net.futureset.kontroldb.DbAwareTemplate
+import net.futureset.kontroldb.SqlTemplate
 import net.futureset.kontroldb.TemplatePriority
 import net.futureset.kontroldb.modelchange.DropIfExists
 import net.futureset.kontroldb.settings.EffectiveSettings
+import org.koin.core.annotation.Singleton
 import kotlin.reflect.KClass
 
+@Singleton(binds=[SqlTemplate::class])
 class DropIfExistsSqlServerTemplate(db: EffectiveSettings) : DbAwareTemplate<DropIfExists>(db, TemplatePriority.DATABASE) {
     override fun type(): KClass<DropIfExists> = DropIfExists::class
 
