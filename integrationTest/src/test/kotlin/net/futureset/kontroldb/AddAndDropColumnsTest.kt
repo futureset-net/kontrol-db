60eaf0f571bf1fb384cd81e96199da74996e4a61
ben@futureset.net <ben@futureset.net>
2023-11-24 20:48:59 +0000

    #10 add and drop columns

---
+++ integrationTest/src/test/kotlin/net/futureset/kontroldb/AddAndDropColumnsTest.kt
@@ -0,0 +1,78 @@
+package net.futureset.kontroldb
+
+import net.futureset.kontroldb.model.StandardColumnTypes.INT_32
+import net.futureset.kontroldb.modelchange.addColumnsTo
+import net.futureset.kontroldb.modelchange.createTable
+import net.futureset.kontroldb.modelchange.dropColumnsFrom
+import net.futureset.kontroldb.refactoring.Refactoring
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+@ExtendWith(DatabaseProvision::class)
+class AddAndDropColumnsTest {
+
+    @Test
+    fun `add a column and drop it`(@DialectName dialectName: String) {
+        KontrolDbEngineBuilder.dsl {
+            loadConfig("test-config.yml")
+            changeModules(
+                module {
+                    refactoring(::CreateATable)
+                    single { AddAColumn(if (dialectName == "sqlserver") "dbo" else "PUBLIC") }.bind(Refactoring::class)
+                },
+            )
+        }.use {
+            assertThat(it.applySql()).isGreaterThan(1)
+            assertThat(
+                it.applySqlDirectly.withConnection { conn ->
+                    conn.executeQuery("SELECT ONE_COLUMN,TWO_COLUMN,THREE_COLUMN FROM FRED") {
+                        ""
+                    }
+                },
+            ).isEmpty()
+        }
+    }
+
+    class CreateATable : Refactoring(
+        executionOrder {
+            ymd(2023, 11, 23)
+            author("ben")
+        },
+        forward = changes {
+            createTable("FRED") {
+                column("ONE_COLUMN", INT_32)
+            }
+        },
+        rollback = emptyList(),
+    )
+
+    class AddAColumn(schemaName: String) : Refactoring(
+        executionOrder {
+            ymd(2023, 11, 24)
+            author("ben")
+            sequence(2)
+        },
+        forward = changes {
+            addColumnsTo("FRED") {
+                column("TWO_COLUMN", INT_32)
+                column("THREE_COLUMN", INT_32)
+                column("FOUR_COLUMN", INT_32)
+                column("FIVE_COLUMN", INT_32)
+                table {
+                    schema(schemaName)
+                }
+            }
+            dropColumnsFrom("FRED") {
+                column("FOUR_COLUMN")
+                column("FIVE_COLUMN")
+                table {
+                    schema(schemaName)
+                }
+            }
+        },
+        rollback = emptyList(),
+    )
+}
---
+++ kontrol-db-core/src/main/kotlin/net/futureset/kontroldb/modelchange/DropColumns.kt
@@ -0,0 +1,38 @@
+package net.futureset.kontroldb.modelchange
+
+import net.futureset.kontroldb.Builder
+import net.futureset.kontroldb.KontrolDbDslMarker
+import net.futureset.kontroldb.ModelChangesBuilder
+import net.futureset.kontroldb.model.DbIdentifier
+import net.futureset.kontroldb.model.SchemaObject
+import net.futureset.kontroldb.model.SchemaObjectBuilder
+
+data class DropColumns(
+    val table: SchemaObject,
+    val columns: List<DbIdentifier>,
+) : ModelChange {
+
+    @KontrolDbDslMarker
+    class DropColumnsBuilder(
+        tableName: String,
+        private val columns: MutableList<DbIdentifier> = mutableListOf(),
+    ) : Builder<DropColumnsBuilder, DropColumns> {
+
+        private var table: SchemaObject = SchemaObject(DbIdentifier(tableName))
+
+        fun table(lambda: SchemaObjectBuilder.() -> Unit) = apply {
+            table = SchemaObjectBuilder(table).apply(lambda).build()
+        }
+
+        fun column(vararg columns: String) = apply {
+            this.columns.addAll(columns.map(::DbIdentifier))
+        }
+
+        override fun build(): DropColumns {
+            return DropColumns(table = table, columns = columns)
+        }
+    }
+}
+
+fun ModelChangesBuilder.dropColumnsFrom(tableName: String, lambda: DropColumns.DropColumnsBuilder.() -> Unit): DropColumns =
+    DropColumns.DropColumnsBuilder(tableName).apply(lambda).build().apply(changes::add)
