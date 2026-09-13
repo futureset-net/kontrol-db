bbb3ec5a967a9e85426e4466bf3e5d15483004a4
futureset <ben@futureset.net>
2026-09-13 22:02:31 +0100

    pin docker images

--- kontrol-db-oracle/build.gradle.kts
+++ kontrol-db-oracle/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 extensions.configure<DbDockerExtension> {
-    imageId.set("gvenzl/oracle-free:latest")
+    imageId.set("gvenzl/oracle-free@sha256:7ed34d0ade89c91a553c9cbe9d42457c758fa29a762aaaa3137541ff30a2d1cb")
     envProperties.put("ORACLE_PASSWORD", "Th1sIsW0rking")
     internalToExternalPortMap.put(1521, 1526)
     waitForStartupLogMessage.set("DATABASE IS READY TO USE!")
